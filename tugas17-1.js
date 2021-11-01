dec1558f6247b27c90666e3c732af34292ff9735
ihlas_sul <ihlassmada@gmail.com>
2021-11-01 08:18:26 +0800

    tugas 17 javascript
    done

---
+++ tugas17-1.js
@@ -0,0 +1,7 @@
+let kata1= 'Saya beLajar bahaSa peMrograman deNgan khUsuk'
+let kata2= 'Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.'
+console.log(kata1.toLowerCase())
+console.log(kata1.toUpperCase())
+console.log('---------------------------------------------------------------------------')
+console.log(kata2.toLowerCase())
+console.log(kata2.toUpperCase())
---
+++ tugas17-2.js
@@ -0,0 +1,2 @@
+let kata = 'Lorem Ipsum'
+console.log(kata.charCodeAt(0)+'-'+kata.charCodeAt(1)+'-'+kata.charCodeAt(2)+'-'+kata.charCodeAt(3)+'-'+kata.charCodeAt(4)+'-'+kata.charCodeAt(5))
